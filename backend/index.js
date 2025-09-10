0c6f8d65ec4ae88808374c2fd92450eae7b448c8
PRIYAM <you@example.com>
2025-09-10 17:35:58 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const connectDB = require("./utils/connectDB");
+const errorHandler = require("./middleware/errorHandler");
+
+dotenv.config();
+connectDB();
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("WORKING");
+});
+
+app.use("/api/transaction", require("./routes/transactions"));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
+});
---
+++ backend/models/transactionModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const transactionModel = mongoose.model("Transaction", transactionSchema);
+
+module.exports = transactionModel;

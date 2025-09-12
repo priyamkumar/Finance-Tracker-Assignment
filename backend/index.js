2b862a1a86787202c006f3aa189e99a0ace9c88a
PRIYAM <you@example.com>
2025-09-13 02:08:24 +0530

    added frontend_url variable in cors origin

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
